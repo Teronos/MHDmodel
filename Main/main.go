95d41ad4e0fc02a29f51f6a978cc4c9384e307b5
KazankovVladislav <v.kazankov98@gmail.com>
2021-12-27 00:11:23 +0300

    master

--- Main/main.go
+++ Main/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"FirstPrototip/Main/lib"
 	"fmt"
+
 	"os/exec"
 )
 
 func main() {
 	fmt.Println("Запустился main.go")
+
 	cmd, _ := exec.Command("python3", "Main/genMass.py").Output()
 	fmt.Println(string(cmd))
 	ar := lib.ProcessingStrTolist2dComplex(cmd)
